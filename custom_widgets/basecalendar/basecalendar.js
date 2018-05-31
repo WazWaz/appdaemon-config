a7b51db7b6f2e106a8261bdb3ed2d5d298c2fea1
WarwickAllison <warwick@allipaca.com>
2018-05-31 10:25:31 +1000

    Push errors to HTML to help user setup.
    Use real time, not rely on setInterval, since setInterval isn't realtime, so if device has been asleep and wakes up, it can take a whole interval to update.

--- custom_widgets/basecalendar/basecalendar.js
+++ custom_widgets/basecalendar/basecalendar.js
@@ -10,10 +10,10 @@ function basecalendar(widget_id, url, skin, parameters)
   
   WidgetBase.call(self, widget_id, url, skin, parameters, monitored_entities, callbacks)  
 
-  self.updateinterval = 600000 // 10 minutes.
+  self.updateinterval = 600 // 10 minutes.
   if ("updateinterval" in parameters)
   {
-    self.updateinterval = parameters.updateinterval * 1000;
+    self.updateinterval = parameters.updateinterval;
   }
 
   self.calendars = {}
@@ -54,19 +54,21 @@ function basecalendar(widget_id, url, skin, parameters)
 
   $.getScript("https://apis.google.com/js/api.js",(function(self){return function(data, textStatus, jqxhr){handleClientLoad(self,data, textStatus, jqxhr)}})(self));
 
+  self.lastTime = -1; 
   self.timerID = setInterval(updateCalendar, 1000, self);
   self.set_field(self, "calendar", "Starting...")
 
   function updateCalendar(self) 
   {
-    clearInterval(self.timerID)
-    if ('undefined' !== typeof gapi && 'undefined' !== typeof gapi.auth2 &&  'undefined' !== gapi.client.calendar && gapi.auth2.getAuthInstance().isSignedIn.get()) {
-      self.timerID = setInterval(updateCalendar, self.updateinterval, self)
-      console.log("Update "+self.updateinterval);
-      listUpcomingEvents(self)
+    if ('undefined' !== typeof gapi && 'undefined' !== typeof gapi.auth2 &&  'undefined' !== typeof gapi.client.calendar && gapi.auth2.getAuthInstance().isSignedIn.get()) {
+      var d = new Date();
+      var t = d.getTime();
+      if (self.lastTime == -1 || t - self.lastTime > self.updateinterval || t < self.lastTime /* clock change */) {
+        listUpcomingEvents(self)
+        self.lastTime = d.getTime();
+      }
     } else {
       self.set_field(self, "calendar", "Login to see calendar...")
-      self.timerID = setInterval(updateCalendar, 1000, self)
     }
   }
 
@@ -103,29 +105,31 @@ function basecalendar(widget_id, url, skin, parameters)
   function initClient(self) {
     console.log( "Init1." );
 
-    gapi.client.init({
-      apiKey: self.API_KEY,
-      clientId: self.CLIENT_ID,
-      discoveryDocs: DISCOVERY_DOCS,
-      scope: SCOPES
-    }).then(function () {
-      // Listen for sign-in state changes.
-      console.log( "Inited." );
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    try {
+      gapi.client.init({
+        apiKey: self.API_KEY,
+        clientId: self.CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+      }).then(function () {
+        // Listen for sign-in state changes.
+        console.log( "Inited." );
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-      // Handle the initial sign-in state.
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-      authorizeButton.onclick = handleAuthClick;
-      signoutButton.onclick = handleSignoutClick;
-      console.log( "Inited2." );
-    },
-    function(error) {
-      console.log(error);
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton.onclick = handleAuthClick;
+        signoutButton.onclick = handleSignoutClick;
+        console.log( "Inited2." );
+      },
+      function(error) {
+        ShowError(self, "Authorization error", error);
+      });
+    } catch (error) {
+      ShowError(self,  "Auth error", error)
     }
-  );
 
     console.log( "Init2." );
-
   }
 
   /**
@@ -180,122 +184,139 @@ function basecalendar(widget_id, url, skin, parameters)
     to.setTime(to.getTime()+1000*60*60*24*self.days);
     self.pending = 0;
     var cal
-    for (cal in self.calendars) {
-      var cparam = self.calendars[cal]
-      self.pending++;
-      console.log("GET "+cal+" "+cparam.color)
-      gapi.client.calendar.events.list({
-        'calendarId': cal,
-        'timeMin': from.toISOString(),
-        'timeMax': to.toISOString(),
-        'showDeleted': false,
-        'singleEvents': true,
-        'maxResults': 100,
-        'fields': "items(id,colorId,start,end,summary,description,location)"
-      }).then((function(self, lcal) {
-        return function(response)
-        {
-        self.calendar.style.display = 'block'
-        self.event.style.display = 'none'
-    
-        var events = response.result.items;
-        if (events.length > 0) {
-          for (var i=0; i<events.length; ++i) {
-            var e = events[i]
-            e.calendar = lcal
-            self.newEntries.push(e)
+    try {
+      for (cal in self.calendars) {
+        var cparam = self.calendars[cal]
+        self.pending++;
+        console.log("GET "+cal+" "+cparam.color)
+        gapi.client.calendar.events.list({
+          'calendarId': cal,
+          'timeMin': from.toISOString(),
+          'timeMax': to.toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 100,
+          'fields': "items(id,colorId,start,end,summary,description,location)"
+        }).then((function(self, lcal) {
+          return function(response)
+          {
+          self.calendar.style.display = 'block'
+          self.event.style.display = 'none'
+      
+          var events = response.result.items;
+          if (events.length > 0) {
+            for (var i=0; i<events.length; ++i) {
+              var e = events[i]
+              e.calendar = lcal
+              self.newEntries.push(e)
+            }
           }
-        }
-        self.pending--;
-        if (self.pending == 0) {
-          var text ="<table>";
-          var weekday = new Array(7);
-          weekday[0] =  "Sunday";
-          weekday[1] = "Monday";
-          weekday[2] = "Tuesday";
-          weekday[3] = "Wednesday";
-          weekday[4] = "Thursday";
-          weekday[5] = "Friday";
-          weekday[6] = "Saturday";
-          var today = new Date();
-          today.setHours(0,0,0,0);
-          var dayms = 1000*60*60*24;
-          var date = -1;
-          self.newEntries.sort(function(a,b){
-            var awhen = a.start.dateTime;
-            if (!awhen) awhen = a.start.date;
-            var bwhen = b.start.dateTime;
-            if (!bwhen) bwhen = b.start.date;
-            //console.log(a.summary+" "+a.start.dateTime+"/"+a.start.date+"|"+awhen+" -> "+new Date(awhen))
-            return (new Date(awhen)).getTime() - (new Date(bwhen)).getTime();
-          });
-          for (i=0; i<self.newEntries.length; ++i) {
-            event = self.newEntries[i];
-            var param = self.calendars[event.calendar]
-            console.log(event.calendar+": "+param.color)
-            var when = event.start.dateTime;
-            if (!when) {
-              when = event.start.date;
-            }
-            var whenend = event.end.dateTime
-            if (!whenend) {
-              whenend = event.end.date
-            }
-            var end = new Date(whenend)
-            event.enddate = end
-            var whendate = new Date(when)
-            event.startdate = whendate
-            var whendate0 = new Date(when)
-            whendate0.setHours(0,0,0,0)
-            if (date+dayms <= whendate0.getTime()) {
-              date = whendate0.getTime()
-              text += "<tr><th colspan=2 align=center class=dayheader>"
-              if (whendate0.getTime() == today.getTime())
-                text += "Today"
-              else if (whendate0.getTime() == today.getTime()+dayms)
-                text += "Tomorrow"
-              else
-                text += weekday[whendate0.getDay()]
-              text += "</th></tr>"
-            }
-            var time = whendate.getMinutes();
-            if (time < 10) time = "0"+time;
-            var hours = whendate.getHours();
-            if (hours >= 12) {
-              if (hours > 12) hours-=12;
-              time = hours+":"+time+"pm";
-            } else {
-              time = hours+":"+time+"am";
-            }
-            var summary = event.summary
-            var now = new Date()
-            var color = self.color
-            if (end < now) {
-              color = self.colorPast
-            } else {
-              if (param && param.color) {
-                color = param.color
+          self.pending--;
+          if (self.pending == 0) {
+            var text ="<table>";
+            var weekday = new Array(7);
+            weekday[0] =  "Sunday";
+            weekday[1] = "Monday";
+            weekday[2] = "Tuesday";
+            weekday[3] = "Wednesday";
+            weekday[4] = "Thursday";
+            weekday[5] = "Friday";
+            weekday[6] = "Saturday";
+            var today = new Date();
+            today.setHours(0,0,0,0);
+            var dayms = 1000*60*60*24;
+            var date = -1;
+            self.newEntries.sort(function(a,b){
+              var awhen = a.start.dateTime;
+              if (!awhen) awhen = a.start.date;
+              var bwhen = b.start.dateTime;
+              if (!bwhen) bwhen = b.start.date;
+              //console.log(a.summary+" "+a.start.dateTime+"/"+a.start.date+"|"+awhen+" -> "+new Date(awhen))
+              return (new Date(awhen)).getTime() - (new Date(bwhen)).getTime();
+            });
+            for (i=0; i<self.newEntries.length; ++i) {
+              event = self.newEntries[i];
+              var param = self.calendars[event.calendar]
+              console.log(event.calendar+": "+param.color)
+              var when = event.start.dateTime;
+              if (!when) {
+                when = event.start.date;
               }
+              var whenend = event.end.dateTime
+              if (!whenend) {
+                whenend = event.end.date
+              }
+              var end = new Date(whenend)
+              event.enddate = end
+              var whendate = new Date(when)
+              event.startdate = whendate
+              var whendate0 = new Date(when)
+              whendate0.setHours(0,0,0,0)
+              if (date+dayms <= whendate0.getTime()) {
+                date = whendate0.getTime()
+                text += "<tr><th colspan=2 align=center class=dayheader>"
+                if (whendate0.getTime() == today.getTime())
+                  text += "Today"
+                else if (whendate0.getTime() == today.getTime()+dayms)
+                  text += "Tomorrow"
+                else
+                  text += weekday[whendate0.getDay()]
+                text += "</th></tr>"
+              }
+              var time = whendate.getMinutes();
+              if (time < 10) time = "0"+time;
+              var hours = whendate.getHours();
+              if (hours >= 12) {
+                if (hours > 12) hours-=12;
+                time = hours+":"+time+"pm";
+              } else {
+                time = hours+":"+time+"am";
+              }
+              var summary = event.summary
+              var now = new Date()
+              var color = self.color
+              if (end < now) {
+                color = self.colorPast
+              } else {
+                if (param && param.color) {
+                  color = param.color
+                }
+              }
+              if (color) {
+                summary = "<font color=\""+color+"\">"+summary+"</font>";
+              }
+              text = text + "<tr><th class=timeheader>"+time+ "</th><td id=\""+event.id+"\">" + summary + "</td></tr>";
             }
-            if (color) {
-              summary = "<font color=\""+color+"\">"+summary+"</font>";
+            text = text + "</table>";
+            self.set_field(self, "calendar", text);
+            for (i=0; i<self.newEntries.length; ++i) {
+              event = self.newEntries[i];
+              var elem = $("#"+event.id)[0]
+              elem.onclick = (function(self,event){return function(){OnEventClick(self,event)}})(self,event);
             }
-            text = text + "<tr><th class=timeheader>"+time+ "</th><td id=\""+event.id+"\">" + summary + "</td></tr>";
           }
-          text = text + "</table>";
-          self.set_field(self, "calendar", text);
-          for (i=0; i<self.newEntries.length; ++i) {
-            event = self.newEntries[i];
-            var elem = $("#"+event.id)[0]
-            elem.onclick = (function(self,event){return function(){OnEventClick(self,event)}})(self,event);
-          }
-        }
-      }})(self,cal),
-      function(error)
-      {
-        console.log("Calendar '"+cal+"': "+error)
-      });
+        }})(self,cal),
+        function(error)
+        {
+          ShowError(self, "Calendar Error", error)
+        });
+      }
+    } catch (error) {
+      ShowError(self, "Calendar API Error", error)
     }
+  }
+
+  function ShowError(self,title,error)
+  {
+    console.log("Error: "+error)
+    self.set_field(self, "event_summary", title)
+    self.set_field(self, "event_location", "")
+    self.set_field(self, "event_start", "")
+    self.set_field(self, "event_end", "")
+    self.set_field(self, "event_description", JSON.stringify(error))
+    self.calendar.style.display = 'none'
+    self.event.style.display = 'block'
+    self.autoback = setInterval(OnBackButton, 6000000, self)
   }
 
   function OnEventClick(self,event)
